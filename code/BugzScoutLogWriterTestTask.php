84b2a9b8892bb1b9fc3b225dff42283fc5e618d8
Sander van Dragt <sander@vandragt.com>
2015-07-14 10:38:53 +0100

    NEW list LogWriter instances on test page, improve instructions.

--- code/BugzScoutLogWriterTestTask.php
+++ code/BugzScoutLogWriterTestTask.php
@@ -9,14 +9,20 @@ class BugzScoutLogWriterTestTask extends BuildTask {
 
 	public function run($request) {
 		$writers = SS_Log::get_writers();
+		print("<pre>The following log writers have been registered:" . PHP_EOL);
 		foreach ($writers as $w) {
+			printf(" * %s" . PHP_EOL, get_class($w));
 			if (get_class($w) == 'BugzScoutLogWriter') {
-				break;
+				$lw = $w;
 			}
 		}
-		Debug::dump($w);
-		printf('This task forces an error. If configured correctly, a new bug will be opened in <a href="%s">%s</a>.', $w->host(), $w->host());
-		user_error('test error');
+		if ($lw) {
+			printf('</pre><p>This task forces an error. If configured correctly, a new bug will be opened in <a href="%s">%s</a>.</p>', $lw->host(), $lw->host());
+			user_error('test error');
+		}
+		else {
+			print("</pre><p>No BugzScoutLogWriter instances found. Please review your setup.</p>");
+		}
 	}
 
 }

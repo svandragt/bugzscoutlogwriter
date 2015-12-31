adbcc1d8705ca5576c411acf9d350eb8b6afc81f
helpfulrobot <chris@silverstripe.com>
2016-01-01 02:21:53 +1300

    Converted to PSR-2

--- code/BugzScoutLogWriter.php
+++ code/BugzScoutLogWriter.php
@@ -10,56 +10,59 @@ require_once 'Zend/Log/Writer/Abstract.php';
  * @package framework
  * @subpackage dev
  */
-class BugzScoutLogWriter extends Zend_Log_Writer_Abstract {
+class BugzScoutLogWriter extends Zend_Log_Writer_Abstract
+{
 
-	protected $description;
-	protected $extra;
-	protected $host;
-	protected $scoutUserName;
-	protected $scoutProject;
-	protected $scoutArea;
+    protected $description;
+    protected $extra;
+    protected $host;
+    protected $scoutUserName;
+    protected $scoutProject;
+    protected $scoutArea;
 
-	public function __construct($config ) {
-		$this->host          = $config['host'];		
-		$this->scoutUserName = $config['scoutUserName'];		
-		$this->scoutProject  = $config['scoutProject'];		
-		$this->scoutArea     = $config['scoutArea'];		
-	}
+    public function __construct($config)
+    {
+        $this->host          = $config['host'];
+        $this->scoutUserName = $config['scoutUserName'];
+        $this->scoutProject  = $config['scoutProject'];
+        $this->scoutArea     = $config['scoutArea'];
+    }
 
-	public function host() {
-		return $this->host;
-	}
+    public function host()
+    {
+        return $this->host;
+    }
 
-	
-	static public function factory($config) {
-		return new BugzScoutLogWriter($config );
-	}
+    
+    public static function factory($config)
+    {
+        return new BugzScoutLogWriter($config);
+    }
 
-	public function _write($event) {
+    public function _write($event)
+    {
+        if (!$this->_formatter) {
+            $formatter = new LogErrorBugzScoutFormatter();
+            $this->setFormatter($formatter);
+        }
 
-		if(!$this->_formatter) {
-			$formatter = new LogErrorBugzScoutFormatter();
-			$this->setFormatter($formatter);
-		}
+        $formattedData     = $this->_formatter->format($event);
+        $this->description = $formattedData['subject'];
+        $this->extra       = $formattedData['data'];
 
-		$formattedData     = $this->_formatter->format($event);
-		$this->description = $formattedData['subject'];
-		$this->extra       = $formattedData['data'];
+        $cache_expiry = 1;
+        $request = new RestfulService($this->host . "/scoutSubmit.asp", $cache_expiry);
 
-		$cache_expiry = 1;
-		$request = new RestfulService($this->host . "/scoutSubmit.asp", $cache_expiry);
+        $params = array(
+            'ScoutUserName' => $this->scoutUserName,
+            'ScoutProject'  => $this->scoutProject,
+            'ScoutArea'     => $this->scoutArea,
+            'Description'   => $this->description,
+            'Extra'         => $this->extra,
+        );
 
-		$params = array(
-			'ScoutUserName' => $this->scoutUserName,
-			'ScoutProject'  => $this->scoutProject,
-			'ScoutArea'     => $this->scoutArea,
-			'Description'   => $this->description,
-			'Extra'         => $this->extra,
-		);
-
-		$conn = $request->request(null, 'POST', $params, null,array(
-			CURLOPT_CONNECTTIMEOUT => 10,
-		));
-	}
-
+        $conn = $request->request(null, 'POST', $params, null, array(
+            CURLOPT_CONNECTTIMEOUT => 10,
+        ));
+    }
 }
